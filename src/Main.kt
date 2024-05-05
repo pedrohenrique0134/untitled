1a5a221fd6ae4d0042d8259a87b7796a3665961b
pedrohenrique0134 <73433708+pedrohenrique0134@users.noreply.github.com>
2024-05-04 21:40:56 -0300

    estudos

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
-fun main() {
-    println("Hello World!")
+
+fun cadastrar(email:String, password:String):String{
+     return when{
+             email.isBlank() && password.isBlank() -> "Preencha todos los campos"
+             email.isBlank() -> "digite um email"
+             password.isBlank() -> "digite uma senha"
+
+            else -> "email cadastrado com sucesso"
+     }
+}
+
+fun main(){
+    var email = ""
+    var password = "123456"
+
+    var resu = cadastrar(email,password)
+    println(resu)
 }
